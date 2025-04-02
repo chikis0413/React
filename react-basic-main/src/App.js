e5e3d945513eb261723392f7e1042e007de409d0
chikis0413 <141794603+chikis0413@users.noreply.github.com>
2025-04-02 01:50:51 +0000

    General

---
+++ react-basic-main/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+// programar el contenido de la 
+// pagina
+
+function App() 
+{
+
+    const value = 'World';
+    return <div>Hello {value}</div>;
+
+
+}
+
+export default App;
---
+++ react-basic-main/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// se3 realiza un import de un 
+// archivo llamado App que 
+// se comporta como un
+// componente
+
+import App from './App';
+
+
+ReactDOM.render(<App />, 
+document.getElementById('root'));
+
+
+
